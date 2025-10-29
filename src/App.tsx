3b44295246e5f7ae8efb481dc68129dfb086c5f9
vindex1810 <viniciussilva1810@gmail.com>
2025-10-29 01:09:04 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -110,23 +110,5 @@ function App() {
     </div>
   );
 }
-useEffect(() => {
-  // Remover elementos do Bolt
-  const removeBoltElements = () => {
-    const elements = document.querySelectorAll('*');
-    elements.forEach(el => {
-      if (
-        el.innerHTML?.includes('bolt.host') ||
-        el.src?.includes('bolt') ||
-        el.className?.includes('bolt')
-      ) {
-        el.remove();
-      }
-    });
-  };
 
-  removeBoltElements();
-  setTimeout(removeBoltElements, 1000);
-  setTimeout(removeBoltElements, 3000);
-}, []);
 export default App;
