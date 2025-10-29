883a575ad7d4b3c67dcaa248d8516e31b412e04a
vindex1810 <viniciussilva1810@gmail.com>
2025-10-29 02:33:19 -0400

    Refactor Vite config for simplified build output

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  root: 'susconnect/',
   build: {
-    outDir: '../../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
-  },
-  server: {
-    port: 3000
+    outDir: 'dist'
   }
 })
